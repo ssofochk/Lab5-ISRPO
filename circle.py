80db5f2e3fdbd13b08063a775fb672248f8ac6fd
ssofochk <ssofochk@gmail.com>
2025-10-17 10:53:38 +0300

    Add comments to square.py and circle.py. Add rectangle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+'''Принимает радиус круга, возвращает его площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+'''Принимает радиус круга, возвращает длину его окружности'''
     return 2 * math.pi * r
 
